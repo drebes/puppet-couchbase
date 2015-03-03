138677cc49b25383468b3563eb936cf00265b74f
Roberto JUNG DREBES <roberto.drebes@amadeus.com>
2015-03-03 16:56:11 +0100

    playing with zones

--- lib/facter/cluster_size.rb
+++ lib/facter/cluster_size.rb
@@ -1,14 +1,14 @@
 Facter.add(:cluster_size) do
       setcode do
         if File.exist? '/usr/local/bin/couchbase-cluster-server-list'
-          Facter::Core::Execution.exec('/usr/local/bin/couchbase-cluster-server-list | wc --lines')
+          Facter::Core::Execution.exec('/usr/local/bin/couchbase-cluster-server-list | grep 'server:' | wc --lines')
         end
       end
-    end
+end
 
-    # If this server doesn't look like a server, it must be a desktop
-         Facter.add(:cluster_size) do
-           setcode do
-                      '0'
-                            end
-                                end
+# If this server doesn't look like a server, it must be a desktop
+Facter.add(:cluster_size) do
+  setcode do
+    '0'
+  end
+end
